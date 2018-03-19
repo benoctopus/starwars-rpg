b5229493ff542e595648359dd9b317d91829f7b3
benjamin rose <rose.benjamin925@gmail.com>
2018-03-19 01:13:39 -0700

    added framework for item repositioning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
 class Jedi {
   //object template for jedi
-  constructor(name, src, health, baseAttack, counterAttack) {
+  constructor(name, value, src, health, baseAttack, counterAttack) {
     this.name = name;
+    this.value = value;
     this.health = health;
+    this.baseHealth = health;
+    this.attack = baseAttack;
     this.baseAttack = baseAttack;
-    this.currentAttack = this.baseAttack;
     this.counterAttack = counterAttack;
     this.src = src;
     this.createElement()
@@ -14,26 +16,26 @@ class Jedi {
     //jedi element creation for html
     //container element
     this.element = $("<button>");
-    this.element.addClass(".heroBox col-3 card");
-    this.element.attr("value", this.name);
+    this.element.addClass("col-3 justify-content-center heroBox");
+    this.element.attr("value", this.value);
     //character title
     this.title = $("<h3>");
-    this.title.addClass(".heroT card-header w-100");
+    this.title.addClass("heroT w-100");
     this.title.text(this.name);
     this.element.append(this.title);
     //image placement
     this.image = $("<img>");
     this.image.attr("src", this.src);
     this.image.attr("alt", this.name);
-    this.image.addClass("pic card-image-top");
+    this.image.addClass("pic ");
     this.element.append(this.image);
     //text content holder
     this.content = $("<div>");
-    this.content.addClass("card-body");
+    this.content.addClass(".holder");
     this.element.append(this.content);
     //character status
     this.status = $("<h4>");
-    this.status.addClass(".heroS");
+    this.status.addClass("heroS");
     this.status.text("health: " + this.health);
     this.content.append(this.status)
   }
@@ -42,52 +44,112 @@ class Jedi {
     this.status.text(this.health)
   }
 
-
+  reset() {
+    this.health = this.baseHealth;
+    this.attack = this.baseAttack;
+  }
 }
 
 window.gameEnv = {
-  listener: $(".heroBox"),
+
+  heroBoxes: $(".heroBox"),
+  instructionText: $("#instructions"),
+  secondRow: $("#secondRow"),
+  thirdRow: $("#thirdRow"),
+  fourthRow: $("#fourthRow"),
   wins: 0,
   losses: 0,
+  phase: 0,
 
-  createJedi: function () {
-    // create all jedi
-    this.characters = {};
-    this.characters.luke = new Jedi("Luke Skywalker", "#", 100, 100, 100);
-    this.characters.obi = new Jedi("Obi-Wan Kenobi", "#", 100, 100, 100);
-    this.characters.maul = new Jedi("Darth Maul", "#", 100, 100, 100);
-    this.characters.sidious = new Jedi("Darth Sidious", "#", 100, 100, 100);
-    this.initialDisplay()
+  characters: {
+    luke: new Jedi("Luke Skywalker", "luke", "#", 100, 100, 100),
+    obi: new Jedi("Obi-Wan Kenobi", "obi", "#", 100, 100, 100),
+    maul: new Jedi("Darth Maul", "maul", "#", 100, 100, 100),
+    sidious: new Jedi("Darth Sidious", "sidious", "#", 100, 100, 100),
   },
 
-  initialDisplay: function(){
-    $.each(this.characters, function(key, value) {
-      $("#firstRow").append(value.element);
-    })
+  displaySet: [
+    function() {
+      gameEnv.instructionText.text("Choose your character");
+      gameEnv.secondRow.append(gameEnv.createHeader("Characters"));
+      gameEnv.thirdRow.append(gameEnv.createHeader("   "));
+      let i = 0;
+      $.each(gameEnv.characters, function (key, value) {
+        if( i < 2) {
+          $("#secondRow").append(value.element);
+        }
+        else {
+          $("#thirdRow").append(value.element);
+        }
+        i++
+      });
+    },
+
+    function () {
+      gameEnv.secondRow.append(gameEnv.createHeader("You Chose:"));
+      gameEnv.thirdRow.append(gameEnv.createHeader("Opponents:"));
+      gameEnv.fourthRow.append(gameEnv.createHeader("Defeated:"));
+      for (let [index, [obj, status]] of gameEnv.activeSet.entries()) {
+        console.log(obj);
+        switch (status) {
+          case "player":
+            gameEnv.secondRow.append(obj.element);
+            break;
+          case "standBy":
+            gameEnv.thirdRow.append(obj.element);
+            break;
+          case "dead":
+            gameEnv.fourthRow.append(obj.element);
+            break;
+        }
+      }
+    }
+  ],
+
+  createHeader: function(text) {
+    let header = $("<h5>");
+    header.addClass("col-12");
+    header.text(text);
+    return header;
   },
 
-  // dispatcher: function(val) {
-  //   switch (this.state) {
-  //     case "pre":
-  //       this.chooseJedi(val);
-  //       break;
-  //     case "chooseOpponent":
-  //       this.chooseOpponent(val);
-  //       break;
-  //   }
-  // },
+  chooseCharacter: function(val){
+    console.log(val);
+    console.log("here");
+    this.activeSet = [];
+    $.each(this.characters, function (key, value) {
+      if (key === val) {
+        gameEnv.activeSet.push([value, "player"])
+      }
+      else {
+        gameEnv.activeSet.push([value, "standBy"])
+      }
+    });
+    this.phase = 1;
+    this.display();
+  },
+
+  display: function() {
+    $(".envo").empty();
+    this.displaySet[this.phase]()
+  },
+
+  boxClick: function(val) {
+    if(this.phase === 0) {
+      this.chooseCharacter(val)
+    }
+  },
 
   initialize: function () {
-    this.createJedi();
-    this.state = "pre";
+    this.display();
+    $(".heroBox").click(function () {
+      let value = $(this).val();
+      console.log(value);
+      gameEnv.boxClick(value);
+    });
   }
 };
 
 $(document).ready(function () {
   gameEnv.initialize();
-  gameEnv.listener.on("click", function (event) {
-    let val = gameEnv.listener.val();
-    console.log(val);
-    // gameEnv.dispatcher(this.val());
-  });
 });
